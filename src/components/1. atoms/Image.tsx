f799dd6862a430ecd7939cbc83ce7e2e70d82cd1
thecodeofyamid <yhrodriguez1@hotmail.com>
2024-12-18 20:25:08 -0500

    Ajustes molecula group header

--- src/components/1. atoms/Image.tsx
+++ src/components/1. atoms/Image.tsx
@@ -1,23 +1,23 @@
 import React from 'react';
 
 interface ImageProps {
-    src: string;
-    alt: string;
-    width?: number;
-    height?: number;
-    className?: string;
+  src: string;
+  alt: string;
+  width?: string;
+  height?: string;
+  className?: string;
 }
 
 const Image: React.FC<ImageProps> = ({ src, alt, width, height, className }) => {
-    return (
-        <img 
-            src={src} 
-            alt={alt} 
-            width={width} 
-            height={height} 
-            className={className} 
-        />
-    );
+  return (
+    <img
+      src={src}
+      alt={alt}
+      width={width}
+      height={height}
+      className={className}
+    />
+  );
 };
 
 export default Image;
--- src/components/2. molecules/Group.tsx
+++ src/components/2. molecules/Group.tsx
@@ -1,18 +1,21 @@
 import React from 'react';
 import Image from '../1. atoms/Image';
 import Paragraph from '../1. atoms/Paragraph';
+import logo from '../../assets/yamid-tech.png'
 
 const Group: React.FC = () => {
-    return (
-        <div className="group">
-            <Image src={src} 
-            alt={alt} 
-            width={width} 
-            height={height} 
-            className={className} ></Image>
-            <Paragraph></Paragraph>
-        </div>
-    );
+  return (
+    <div className="group">
+      <Image src={logo}
+        alt="logo yamid tech"
+        width="100"
+        height="100"
+        className="logo" />
+      <Paragraph
+        text="Yamid Dev"
+      />
+    </div>
+  );
 };
 
 export default Group;

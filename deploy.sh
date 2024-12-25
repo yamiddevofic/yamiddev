734c4c5586fab749e99b8ff30254ecad39be3054
thecodeofyamid <yhrodriguez1@hotmail.com>
2024-12-25 16:17:41 -0500

    Cree pagina de mantenimiento y archivo de despliegue automatico

---
+++ deploy.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Asegúrate de estar en la rama principal
+git checkout main
+
+# Construir el proyecto (genera la carpeta dist)
+npm run build
+
+# Mueve el contenido de dist a la rama deploy
+git subtree push --prefix dist origin deploy

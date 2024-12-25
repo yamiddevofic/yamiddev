21ec05295b5a2df853c2287bdf2367fef32fb0ce
thecodeofyamid <yhrodriguez1@hotmail.com>
2024-12-25 16:37:32 -0500

    Deploy update: 2024-12-25 16:37:32

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,10 @@
 # Asegúrate de estar en la rama principal
 git checkout main
 
+# Guardar cambios de rama main
+git add .
+git commit -m "Deploy update: $(date '+%Y-%m-%d %H:%M:%S')"
+
 # Construir el proyecto (genera la carpeta dist)
 npm run build
 

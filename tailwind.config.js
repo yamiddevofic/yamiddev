9aad194d5ca0f3f23e143fe9f7da4160b7fc02be
thecodeofyamid <yhrodriguez1@hotmail.com>
2024-12-25 16:17:41 -0500

    Cree pagina de mantenimiento y archivo de despliegue automatico

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.{astro,html,js,jsx,ts,tsx}',
+    './public/**/*.html',
+    './public/**/*.js',
+    './public/**/*.css',
+    '*.html',
+    '*.astro',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};

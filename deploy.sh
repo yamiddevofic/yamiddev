6893eb5dc984814cadcd21eeae94696a551f1fb3
thecodeofyamid <yhrodriguez1@hotmail.com>
2024-12-26 00:19:43 -0500

    config deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
+#!/bin/bash
 set -e  # Detener la ejecución si ocurre algún error
 
 # Configurar usuario de Git
 git config user.name "GitHub Actions"
 git config user.email "actions@github.com"
 
-# Asegurar sincronización con la rama remota (evitar conflictos)
-git fetch origin deploy || true
-git pull origin deploy || true
+# Crear o actualizar la rama deploy
+if git ls-remote --exit-code origin deploy; then
+  # Si la rama deploy existe, eliminarla para evitar conflictos
+  git push origin --delete deploy
+fi
 
 # Subir la carpeta dist a la rama deploy
-git subtree push --prefix dist origin deploy --force
-
+git subtree push --prefix dist origin deploy

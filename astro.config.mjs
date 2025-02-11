382cfd53e5130bc4e60b1fc8403fc0ef9b597937
yamiddev <yhrodriguez1@hotmail.com>
2025-02-11 02:28:09 -0500

    modern version

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
+import icon from "astro-icon";
 
 export default defineConfig({
   integrations: [
     react(),
     tailwind(),
+    icon()
   ],
   vite: {
     plugins: [

d2092bf1c82d6ab2a621571aecedf3a2e70520bc
elverdavid <elverdavid0839@gmail.com>
2025-02-19 23:46:12 -0500

    refactor: refactor button projects

---
+++ src/lib/projectData.ts
@@ -0,0 +1,38 @@
+export   const Projects = [
+  {
+    title: 'Gestor de Tareas',
+    description: 'Aplicación web para gestionar tareas con funcionalidades colaborativas y notificaciones en tiempo real.',
+    techStack: ['React', 'Node.js', 'MongoDB'],
+    image: '/images/gestor-tareas.webp'
+  },
+  {
+    title: 'Plataforma E-commerce',
+    description: 'Tienda online con integración de pagos, gestión de inventario y seguimiento de pedidos.',
+    techStack: ['Next.js', 'Stripe', 'Firebase'],
+    image: 'https://www.inesdi.com/sites/default/files/inline-images/consejos-para-crear-un-ecommerce-de-exito-en-7-pasos.jpeg'
+  },
+  {
+    title: 'Dashboard Analítico',
+    description: 'Panel interactivo para visualizar datos y métricas en tiempo real con gráficos dinámicos.',
+    techStack: ['Vue.js', 'D3.js', 'Express.js'],
+    image: 'https://images.klipfolio.com/website/public/7eab5172-6fe0-4304-9e9d-4750646a9791/powermetrics-dashboard_0.png'
+  },
+  {
+    title: 'App Móvil de Salud',
+    description: 'Aplicación para el seguimiento de la salud y actividades físicas, con integración a dispositivos wearables.',
+    techStack: ['React Native', 'Redux', 'Firebase'],
+    image: 'https://static.k-tuin.com/media/blog/app-salud-para-iphone_1.png'
+  },
+  {
+    title: 'Red Social',
+    description: 'Plataforma para conectar personas con intereses comunes, ofreciendo mensajería segura y feeds personalizados.',
+    techStack: ['Angular', 'Node.js', 'PostgreSQL'],
+    image: 'https://ahoraeg.com/listas/wp-content/uploads/2025/02/Redes-sociales-%E2%80%93-Que-son-tipos-usos-ventajas-y-riesgos.webp'
+  },
+  {
+    title: 'Sitio Web Portafolio',
+    description: 'Sitio personal para exhibir proyectos y habilidades, con diseño moderno y responsive.',
+    techStack: ['Astro', 'TailwindCSS', 'React'],
+    image: 'https://www.hostinger.es/tutoriales/wp-content/uploads/sites/7/2023/04/image-13.png'
+  }
+];

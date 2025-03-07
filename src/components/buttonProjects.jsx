c0a1f807f01cc94eb326853e18bc5ffa57fbdbba
yamiddevofic <yhrodriguez1@gmail.com>
2025-03-07 00:05:04 -0500

    modifed index, nav, and about me section

--- src/components/buttonProjects.jsx
+++ src/components/buttonProjects.jsx
@@ -2,13 +2,12 @@ import { Portfolio } from "./icons/Portfolio";
 
 const buttonProjects = () => {
     return(
-        <div>
+        <div className="w-full flex items-center justify-end">
             <button
             id="projects"
-            class="bg-gradient-to-r from-blue-500 to-emerald-500 text-white flex items-center gap-x-2 py-3 px-16 rounded-2xl transition-all active:scale-90 cursor-pointer font-semibold z-10"
+            class="bg-gradient-to-r from-blue-500 to-emerald-500 text-white flex items-center gap-x-2 py-3 px-2 rounded-2xl transition-all active:scale-90 cursor-pointer font-semibold z-10"
             >
-                <Portfolio />
-                Proyectos
+                Haz click para ver mis proyectos
             </button>
         </div>
     )

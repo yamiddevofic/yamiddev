263a302760a5dd3dd63afaa2f6d7810b92ff4d6a
yamiddevofic <yhrodriguez1@gmail.com>
2025-03-18 19:01:56 -0500

    add project portfolio

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -39,7 +39,7 @@ const Carousel = () => {
       </h2>
       
       <div 
-        className="relative w-full max-w-4xl mx-auto rounded-xl shadow-xl overflow-hidden border border-gray-100 dark:border-gray-700 bg-white dark:bg-gray-900"
+        className="relative w-full max-w-4xl mx-auto rounded-xl  overflow-hidden border border-gray-100 dark:border-gray-700 bg-white dark:bg-gray-900"
         onMouseEnter={() => setIsPaused(true)}
         onMouseLeave={() => setIsPaused(false)}
       >
@@ -97,7 +97,7 @@ const Carousel = () => {
             animate={{ opacity: 1, x: 0 }}
             exit={{ opacity: 0, x: -100 }}
             transition={{ duration: 0.5 }}
-            className="w-full h-auto flex flex-col rounded-xl shadow-lg border border-gray-100 dark:border-gray-700 bg-white dark:bg-gray-900 cursor-pointer"
+            className="w-full h-auto flex flex-col rounded-xl border border-gray-300 dark:border-gray-700 bg-white dark:bg-gray-900 cursor-pointer"
             onClick={() => window.open(Projects[currentIndex].url, '_blank')}
           >
             {/* Imagen con altura fija para mantener el mismo tamaño */}
